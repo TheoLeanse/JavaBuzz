fd07929b8b9db0eac1b8ce9d4fca21800887b103
TheoLeanse <theoleanse@gmail.com>
2015-07-06 15:42:14 +0100

    implements output with isDivisibleBy functions

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -15,3 +15,16 @@ Javabuzz.prototype.isDivisibleByFive = function(number) {
 Javabuzz.prototype.isDivisibleByThree = function(number) {
   return this._isDivisibleBy(number, 3);
 };
+
+Javabuzz.prototype.says = function(number) {
+  if (this.isDivisibleByFifteen(number)) {
+    return "Javabuzz";
+  };
+  if (this.isDivisibleByThree(number)) {
+    return "Java";
+  };
+  if (this.isDivisibleByFive(number)) {
+    return "Buzz";
+  };
+  return number;
+};
